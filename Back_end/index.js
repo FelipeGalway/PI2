e39302f67d849ec32612d9fab82a45668d302492
Felipe Ferreira Rezende <felipe_legitimo@hotmail.com>
2023-11-07 10:31:24 -0300

    PI

--- Back_end/index.js
+++ Back_end/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../Front_end")));
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
+
+
 
