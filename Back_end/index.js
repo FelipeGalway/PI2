fd72fb7e147a601553ef5a2310904eb4c4c9f089
Felipe Ferreira Rezende <felipe_legitimo@hotmail.com>
2023-11-07 15:06:09 -0300

    PI

--- Back_end/index.js
+++ Back_end/index.js
@@ -2,16 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
-const imoveis = require('./imoveis.json'); // Importe o arquivo JSON com os imóveis
+const imoveis = require('./imoveis.json'); 
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../Front_end")));
 
-// Rota para tratar a busca de imóveis
 app.get('/buscar-imoveis', (req, res) => {
   const { location, propertyType, priceRange, priceRangeMax } = req.query;
 
-  // Filtre os imóveis com base nos parâmetros da busca
   const resultados = imoveis.filter((imovel) => {
     return (
       (location ? imovel.endereco.toLowerCase().includes(location.toLowerCase()) : true) &&
@@ -21,7 +19,7 @@ app.get('/buscar-imoveis', (req, res) => {
     );
   });
 
-  res.json(resultados); // Retorne os resultados como JSON
+  res.json(resultados); 
 });
 
 app.listen(port, () => {
