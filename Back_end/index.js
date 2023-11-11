a03666feec43bd1b88c65bf3b01748ef934240e5
Felipe Ferreira Rezende <felipe_legitimo@hotmail.com>
2023-11-11 09:28:23 -0300

    PI

--- Back_end/index.js
+++ Back_end/index.js
@@ -2,7 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
-const imoveis = require('./imoveis.json'); 
+const imoveis = require('./imoveis.json');
+const sql = require('mssql/msnodesqlv8')
+
+const pool = new sql.ConnectionPool({
+  database: 'PI',
+  server: 'localhost',
+  driver: 'msnodesqlv8',
+  options: {
+    trustedConnection: true
+  }
+})
+
+pool.connect().then(() => {  
+  pool.request().query('select 1 as number', (err, result) => {
+        console.dir(result)
+    })
+})
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../Front_end")));
